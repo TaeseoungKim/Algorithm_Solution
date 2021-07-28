6070386591ebec34fbc17df27845ecc5eb330bfb
TaeseoungKim <aka404365@gmail.com>
2021-07-28 23:16:42 +0900

    4693, 2606 문제 해결

--- 0728/test.py
+++ 0728/test.py
@@ -1,9 +1,7 @@
-board = [ [ 1,2,3,4,5] , [2,3,5,50,20] ]
+def hello(board):
+    board.append(10)
 
+board = [ 1,2,3,4 ]
+hello(board)
 
-t_max = []
-for _ in range(2):
-    t_max.append(max(board[_]))
-    
-max_height = max(t_max)
-print(max_height)
+print(board)
