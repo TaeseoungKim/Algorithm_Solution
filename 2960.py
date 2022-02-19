7a09169fcb3c4d9da42c1140d404dfb347087d9c
kimtaeseoung <aka404365@gmail.com>
2022-02-20 04:14:00 +0900

    2960

--- 2960.py
+++ 2960.py
@@ -1,13 +1,11 @@
 import sys
 input = sys.stdin.readline
 n, k = map(int, input().split())
-
 board=[0,0]
-
 for n in range(2,n+1):
     board.append(1)
-cnt=0
-answer=0
+cnt, answer= 0,0
+
 for i in range(len(board)):
     if board[i]==1:
         for d in range(i,n+1,i):
@@ -21,5 +19,3 @@ for i in range(len(board)):
     if answer!=0:
         print(answer)
         break
-                    
-        
