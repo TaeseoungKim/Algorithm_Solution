05521ed61d9b71634e6670e5be1903b2f2d5aa11
kimtaeseoung <aka404365@gmail.com>
2022-02-20 03:29:07 +0900

    2504 수정완료

--- 2504.py
+++ 2504.py
@@ -2,17 +2,36 @@ import sys
 from collections import deque
 input = sys.stdin.readline
 inStr = input()
+
 deq = deque()
-# 재귀함수로 푸는게 날듯하다
-lev = 0
-lev_sum = []
-for c in inStr:
-    if c == '(':
-        lev += 1
-        if len(lev_sum)!=lev:
-            lev_sum.append([])
-        deq.append(2)
-    if c == ')':
-        lev -= 1
-        if  deq.pop() == 2:
-            lev_sum = sum(lev_sum[lev])
+tmp=1
+answer = 0
+for i in range(len(inStr)-1):
+    if inStr[i]=='(':
+        tmp *=2
+        deq.append(inStr[i])
+    elif inStr[i]=='[':
+        tmp *=3
+        deq.append(inStr[i])
+    elif inStr[i]==')':
+        if not deq or deq[-1]=='[':
+            answer=0
+            break
+        if inStr[i-1]=='(':
+            answer += tmp       
+        tmp //=2
+        deq.pop()
+    elif inStr[i]==']':
+        if not deq or deq[-1]=='(':
+            answer=0
+            break
+        if inStr[i-1]=='[':
+            answer += tmp       
+        tmp //=3
+        deq.pop()
+
+
+if deq:
+    print(0)
+else:
+    print(answer)
