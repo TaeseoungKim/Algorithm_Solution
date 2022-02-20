107c88507e3c34ee4caaa8ca9cda58cddb94d27d
kimtaeseoung <aka404365@gmail.com>
2022-02-21 03:32:37 +0900

    1260 리팩토링

--- 1260.py
+++ 1260.py
@@ -1,40 +1,36 @@
+import sys
 from collections import deque
+input = sys.stdin.readline
 
+n, m, v = map(int, input().split())
+m_list = [ [0] * (n+1) for _ in range(n+1) ]
+visit = [0]*(n+1)
+for _ in range(m):
+    n1, n2 = map(int, input().split())
+    m_list[n1][n2] = 1
+    m_list[n2][n1] = 1
 
-def dfs(V):
-    visit[V] = 1
-    print(V,end=" ")
-    for i in range(1,N+1,1):
-        if visit[i] == 0 and nodeList[V][i] == 1:
+def dfs(v):
+    visit[v]=1
+    print(v,end=" ")
+    for i in range(1,n+1):
+        if visit[i] != 1 and m_list[v][i]==1:
             dfs(i)
 
-def bfs(V):
-    deq = deque()
-    deq.append(V)
-    visit[V] = 1  
-    print(V,end=" ")
-    
-    while(deq):
-        node = deq.popleft()
-        for i in range(1,N+1,1):
-            if visit[i] == 0 and nodeList[node][i] == 1:
-                deq.append(i)
-                visit[i] = 1
-                print(i,end=" ")
-                
+bfs_deq = deque()
+def bfs(v):
+    bfs_deq.append(v)
+    visit[v]=1
+    while bfs_deq:
+        p = bfs_deq.popleft()
+        print(p,end=" ")
+            
+        for i in range(1,n+1):
+            if visit[i] != 1 and m_list[p][i]==1:
+                bfs_deq.append(i)
+                visit[i]=1
 
-
-
-N , M , V = map(int, input().split())
-
-nodeList  = [ [0] * (N+1) for i in range(N+1) ] 
-
-for i in range(M):
-     fromNode, toNode = map(int, input().split())
-     nodeList[fromNode][toNode] = nodeList[toNode][fromNode] = 1
-
-visit = [ 0 for i in range(N+1) ]
-dfs(V)
+dfs(v)
+visit = [0]*(n+1)
 print()
-visit = [ 0 for i in range(N+1) ]
-bfs(V)
+bfs(v)
