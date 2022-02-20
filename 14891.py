dbde75378627483a23dc7438fdff42932a14d399
kimtaeseoung <aka404365@gmail.com>
2022-02-21 02:52:34 +0900

    14891

--- 14891.py
+++ 14891.py
@@ -1,30 +1,44 @@
-import sys
 from collections import deque
+import sys
 
 input = sys.stdin.readline
-
-wheel = []
-for _ in range(4):
-    wheel.append(deque(list(input()[:8])))
-k = int(input())
+wheel  = []
 move = []
+for _ in range(4):
+    wheel.append(deque(input()[:8]))
+k = int(input())
 for _ in range(k):
-    move.append(list(map(int,input().split())))
-print(wheel)
-print(move)
-for m, dir in move:
-    print(m,dir)
-    if m in (1,2):
-        if wheel[m-2][2] != wheel[m-1][6]:
-            wheel[m-2].rotate(-dir)
-        if wheel[m][6] != wheel[m-1][2]:
-            wheel[m].rotate(-dir)
-    if m==1:
-        if wheel[m-1][2] != wheel[m+1][6]:
-            wheel[m+1].rotate(-dir)
+    move.append(tuple(map(int, input().split())))
 
+cnt = 0
+def wheelmove(idx,dir,LeftOrRight):
+    global cnt
+    cnt += 1
 
+    # 톱니바퀴 범위를 벗어났을 때,(1~4)
+    if idx == -1 or idx == 4:
+        return
+    if LeftOrRight == -1:
+        if wheel[idx][2]!=wheel[idx+1][6]:
+            wheelmove(idx-1,-dir,-1)
+            wheel[idx].rotate(-dir)
+    elif LeftOrRight == 1:
+        if wheel[idx][6]!=wheel[idx-1][2]:
+            wheelmove(idx+1,-dir,1)
+            wheel[idx].rotate(-dir)
 
-
+for idx, dir in move:
+    wheelmove(idx-2,dir,-1)
+    wheelmove(idx,dir,1)
+    wheel[idx-1].rotate(dir)
     
-        
+result = 0
+if wheel[0][0]=='1':
+    result += 1
+if wheel[1][0]=='1':
+    result += 2
+if wheel[2][0]=='1':
+    result += 4
+if wheel[3][0]=='1':
+    result += 8
+print(result)
--- test.py
+++ test.py
@@ -1,2 +1,4 @@
-lev = [[]]
-print(len(lev))
+lev = 'gimothi'
+for _ in range(len(lev)):
+    lev[_] = '1'
+print(lev)
