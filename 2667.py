2aa14ac066e7da9c44f5b65ae183006d4a0d74ce
kimtaeseoung <aka404365@gmail.com>
2022-02-21 04:12:22 +0900

    2667 리팩토링

--- 2667.py
+++ 2667.py
@@ -1,49 +1,41 @@
 from collections import deque
-
-def bfs(x,y):
-
-    if board[y][x] == 0 :
-        return
-
-    board[y][x] = 0
-    deq = deque()
-    deq.append( (x,y) )    
-    count = 1
-
-    while deq.__len__() != 0 :
-        pop_x , pop_y = deq.popleft()
-
-        for i in range(4):
-            tmp_x = pop_x + dx[i]
-            tmp_y = pop_y + dy[i]
-
-            if ( 0 <= tmp_x < n and 0 <= tmp_y < n ) != True:
-                continue
-            elif board[tmp_y][tmp_x] == 1 :
-                board[tmp_y][tmp_x] = 0
-                deq.append( (tmp_x,tmp_y) )
-                count += 1
-
-    house_list.append(count)
+import sys
+input = sys.stdin.readline
 
 n = int(input())
+board = []
+visit = list( [0]*n for _ in range(n) )
+deq = deque()
+for _ in range(n):
+    board.append(list(input()[:n]))
+move = [(1,0),(-1,0),(0,1),(0,-1)]
+result = []
 
-board = [ list(map(int, input())) for _ in range(n) ]  #체크
-#board = [ [0,1,1,0,1,0,0] , [1,1,1,0,1,0,0] ,[0,1,1,0,1,0,0] , [0,1,1,0,1,0,0], [0,1,1,0,1,0,0], [0,1,1,0,1,0,1], [0,1,1,0,1,0,0] ]
-house_list = []
+def bfs(x,y):
+    if visit[x][y]==1:
+        return
+    global n
+    cnt=0
+    visit[x][y]=1
+    deq.append((x,y))
+    while deq:
+        x,y = deq.pop()
+        visit[x][y]=1
+        cnt += 1
 
-dy = [ 1 , -1 , 0 , 0 ]
-dx = [ 0 , 0 , -1 , 1 ]
+        for dx,dy in move:
+            if not ((0 <= x+dx <= n-1) and (0 <= y+dy <= n-1)):
+                continue
+            elif visit[x+dx][y+dy]==0 and board[x+dx][y+dy]=='1':
+                deq.append((x+dx,y+dy))
+                visit[x+dx][y+dy]=1
+    result.append(cnt) 
 
 for x in range(n):
     for y in range(n):
-        bfs(x,y)
-
-house_list.sort()
-
-print(house_list.__len__())
-for _ in range(house_list.__len__()):
-    print( house_list[_] )
-                
-                
-
+        if board[x][y]=='1':
+            bfs(x,y)
+result.sort()
+print(len(result))
+for _ in range(len(result)):
+    print(result[_])
