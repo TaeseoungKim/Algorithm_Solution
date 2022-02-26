5a9f186e1dbb10122e045501b57771a3354098f2
kimtaeseoung <aka404365@gmail.com>
2022-02-27 02:07:52 +0900

    1967 stack ver

--- 1967_stackver.py
+++ 1967_stackver.py
@@ -1,6 +1,5 @@
 from collections import deque
 import sys
-sys.setrecursionlimit(10**9)
 
 input = sys.stdin.readline
 n = int(input())
@@ -11,12 +10,16 @@ for _ in range(n-1):
     board[y].append([x,w])
 
 def dfs(v,wei):
-    visit[v]=1
-    dist.append((wei,v))
-    for tmp_v,tmp_wei in board[v]:
-        if visit[tmp_v]==0:
-            dfs(tmp_v,wei+tmp_wei)
-    
+    deq = deque()
+    deq.append((v,wei))
+    while deq:
+        pop_v, pop_wei = deq.pop()
+        visit[pop_v]=1
+        dist.append((pop_wei,pop_v))
+        for tmp_v,tmp_wei in board[pop_v]:
+            if visit[tmp_v]==0:
+                deq.append((tmp_v,pop_wei+tmp_wei))
+
 dist = []
 visit = [0]*(n+1)
 dfs(1,0)
