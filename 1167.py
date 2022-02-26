fedf5fefaeaaae48b457cc5cba218df149bffb5d
kimtaeseoung <aka404365@gmail.com>
2022-02-27 02:56:06 +0900

    1167.py refactoring

--- 1167.py
+++ 1167.py
@@ -1,6 +1,19 @@
 from collections import deque
 import sys
 
+def dfs(v,wei):
+    max_v,i=wei,v
+    deq = deque()
+    deq.append((v,wei))
+    while deq:
+        pop_v, pop_wei = deq.pop()
+        visit[pop_v]=1
+        for tmp_v,tmp_wei in board[pop_v]:
+            if visit[tmp_v]==0:
+                deq.append((tmp_v,pop_wei+tmp_wei))
+                if pop_wei+tmp_wei > max_v:
+                    max_v, i = pop_wei+tmp_wei, tmp_v
+    return max_v,i
 input = sys.stdin.readline
 n = int(input())
 board = [[] for _ in range(n+1)]
@@ -9,24 +22,8 @@ for i in range(n):
     for d in range(1,len(tmp)-2,2):
         board[tmp[0]].append([tmp[d],tmp[d+1]])
 
-def dfs(v,wei):
-    deq = deque()
-    deq.append((v,wei))
-    while deq:
-        pop_v, pop_wei = deq.pop()
-        visit[pop_v]=1
-        dist.append((pop_wei,pop_v))
-        for tmp_v,tmp_wei in board[pop_v]:
-            if visit[tmp_v]==0:
-                deq.append((tmp_v,pop_wei+tmp_wei))
-
-dist = []
 visit = [0]*(n+1)
-dfs(1,0)
-max_Value = max(dist)
-max_Index = dist[dist.index(max_Value)][1]
-
-dist = []
+max_v, i = dfs(1,0)
 visit = [0]*(n+1)
-dfs(max_Index,0)
-print(max(dist)[0])
+max_v, i = dfs(i,0)
+print(max_v)
