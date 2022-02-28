e2bd2827dd0342d5473e49e7b00ae622544677ea
kimtaeseoung <aka404365@gmail.com>
2022-02-28 16:57:01 +0900

    12851,14226

--- test.py
+++ test.py
@@ -1,3 +1,15 @@
-#이게 되네
-te = [1,2,3,4,5]
-print(te[-2])
+
+from collections import deque
+
+
+deq1 = deque([1,[0]])
+deq2 = deque((1,0))
+deq3 = deque()
+
+deq3.append((1,0))
+deq3.append((1,0))
+
+print(deq1.pop())
+print(deq1.pop())
+print(deq2.pop())
+print(deq3.pop())
