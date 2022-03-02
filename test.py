4ea0187468e1f7c6616fc61511d65f650a12c938
kimtaeseoung <aka404365@gmail.com>
2022-03-03 00:51:40 +0900

    2290.py

--- test.py
+++ test.py
@@ -1,15 +1,2 @@
-
-from collections import deque
-
-
-deq1 = deque([1,[0]])
-deq2 = deque((1,0))
-deq3 = deque()
-
-deq3.append((1,0))
-deq3.append((1,0))
-
-print(deq1.pop())
-print(deq1.pop())
-print(deq2.pop())
-print(deq3.pop())
+test = {1:3}
+print(test[1])
