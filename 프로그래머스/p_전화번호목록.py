3e88374f8393cb3490d8f1b6664323bfb7be7c40
kimtaeseoung <aka404365@gmail.com>
2022-03-04 21:53:46 +0900

    p_전화번호목록.py

---
+++ 프로그래머스/p_전화번호목록.py
@@ -0,0 +1,14 @@
+from turtle import left
+
+
+def solution(phone_book):
+    phone_book.sort(key=len)    
+    phone_length = list(map(len, phone_book))
+    list_length = len(phone_book)
+    for i in range(list_length):
+        for d in range(i+1,list_length):
+            if phone_book[i][:phone_length[i]] == phone_book[d][:phone_length[i]]:
+                return False
+    return True
+phone_book = ["119", "97674223", "1195524421"]	
+print(solution(phone_book))
