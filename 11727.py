a6699df1e9b8f8378aed3334bf0cd45cc9437be8
kimtaeseoung <aka404365@gmail.com>
2022-03-07 23:48:46 +0900

    11727

--- 11727.py
+++ 11727.py
@@ -1,7 +1,7 @@
 n=int(input())
-board = [0]*1001
+board = [0]*(n+1)
 board[1]=1
-board[2]=2
+board[2]=3
 for i in range(3,n+1):
-    board[i]=board[i-1]+board[i-2]
+    board[i]=board[i-1]+board[i-2]*2
 print(board[n]%10007)
