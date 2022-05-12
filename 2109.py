846f226379b133a98c9f20c5d527118ea7e7a997
kimtaeseoung <aka404365@gmail.com>
2022-05-12 18:52:37 +0900

    2109.py

---
+++ 2109.py
@@ -0,0 +1,20 @@
+import sys
+import heapq
+input = sys.stdin.readline
+n = int(input())
+board = []
+answ=0
+
+for _ in range(n):
+    board.append(list(map(int, input().split())))
+board.sort(key=lambda x: x[1])
+heap = []
+
+for i in range(n):
+    heapq.heappush(heap, board[i])
+    if len(heap)>board[i][1]:
+        heapq.heappop(heap)
+
+for i in range(len(heap)):
+    answ+=heapq.heappop(heap)[0]
+print(answ)    
