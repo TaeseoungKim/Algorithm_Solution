1f0236ea2bb3ef88d5d434676235fae9fa1c5497
Taeseong <aka404365@gmail.com>
2023-02-28 13:50:58 +0900

    0228

--- 자바스크립트/백준/11725.js
+++ 자바스크립트/백준/11725.js
@@ -2,20 +2,34 @@ const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString().split("\n");
 
+class node {
+  constructor() {
+    this.parent = null;
+  }
+}
+// 아 처음부터 bfs로 접근해야 하는 문제였음.. 접근 순서가 중요하기 떄문이다
 let n = Number(input[0]);
-let T = [];
-let P = [];
+let edges = Array.from(Array(n + 1), () => Array());
+let nodes = Array.from(Array(n + 1), () => new node());
 
-let board = new Array(n + 1).fill(0);
+for (let i = 1; i < n; i++) {
+  [x, y] = input[i].split(" "); // 구조분해 할당
+  edges[x].push(y);
+  edges[y].push(x);
+}
+
 for (let i = 1; i <= n; i++) {
-  let [t, p] = input[i].split(" ").map((value) => Number(value));
-  T.push(t);
-  P.push(p);
+  console.log(`edges${i}:`, edges[i]);
+  for (let d = 0; d < edges[i].length; d++) {
+    if (nodes[edges[i][d]].parent === null && edges[i][d] != 1) {
+      nodes[edges[i][d]].parent = i;
+      console.log(`edges${edges[i][d]}의 부모는:${i}`);
+    }
+  }
+  console.log();
 }
 
-for (let i = n - 1; i >= 0; i--) {
-  if (n < i + T[i]) board[i] = board[i + 1];
-  else board[i] = Math.max(board[i + 1], P[i] + board[i + T[i]]);
+console.log("nodes", nodes);
+for (let i = 2; i <= n; i++) {
+  console.log(nodes[i].parent);
 }
-
-console.log(Math.max(...board));
