707b31a769cc090b711eeba335be982b58c63ddb
Taeseong <aka404365@gmail.com>
2023-05-12 16:52:26 +0900

    0512

--- 파이썬/1339v2.py
+++ 파이썬/1339v2.py
@@ -1,16 +1,33 @@
 import sys
 input = sys.stdin.readline
-
-
 TestCase = int(input())
 board = []
 alphaDict = dict()
 
+
 for i in range(TestCase):
     word = input().strip()
     board.append(word)
-    for c in word:
-        if c in alphaDict:
-            continue
-        else:
+    wordLen = len(word)
+
+    for index, c in enumerate(word):
+        if c not in alphaDict:
             alphaDict[c] = 0
+        alphaDict[c] += 10**(wordLen-(index+1))
+
+alphaTupleList = []
+for key in alphaDict.keys():
+    alphaTupleList.append((key, alphaDict[key]))
+
+alphaTupleList.sort(key=lambda x: -x[1])
+keyLen = len(alphaDict.keys())
+
+curIdx = 0
+sumVal = 0
+for i in range(9, 9-keyLen, -1):
+    _, Value = alphaTupleList[curIdx]
+    sumVal += Value*i
+    curIdx += 1
+
+
+print(sumVal)
