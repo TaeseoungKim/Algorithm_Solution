760d9e24fd23b01e111208c5f0ba4491a8c3df38
KIMTAESEONG <aka404365@gmail.com>
2023-08-24 14:25:37 +0900

    0824

--- 파이썬/1005.py
+++ 파이썬/1005.py
@@ -1,43 +1,37 @@
-# 수정 필요
 import sys
 from collections import deque
 input = sys.stdin.readline
 
-
-def dfs(cur, time):
-    global maxV
-    if cur == W:
-        # maxV = max(maxV, time+Times[cur-1])
-        return
-
-    for edge in edges[cur]:
-        newTime = time+Times[edge-1]
-        if maxArr[edge] < newTime:
-            maxArr[edge] = newTime
-            dfs(edge, newTime)
-
-
 T = int(input())
-
 for i in range(T):
+    answer = 0
+    deq = deque()
     N, K = map(int, input().split())
-    Times = list(map(int, input().split()))
-    edges = [[] for i in range(N+1)]
-    checks = [False for i in range(N+1)]
-    checks[0] = True
-    maxArr = [0 for i in range(N+1)]
-    maxV = 0
-
-    for i in range(K):
-        t1, t2 = map(int, input().split())
-        edges[t1].append(t2)
-        checks[t2] = True
-    # edges 연결된게 없는거를 시작점으로 잡는다.
-
-    S = checks.index(False)
+    timeToBuild = list(map(int, input().split())) # 0부터 시작하는 거 기억
+    board = [[] for i in range(N+1)]
+    degree = [0 for i in range(N+1)]
+    visited = [0 for i in range(N+1)]
+    for d in range(K):
+        X, Y = map(int, input().split())
+        board[X].append(Y)
+        degree[Y] += 1
     W = int(input())
-    if checks[W] == False:
-        print("정답", Times[W-1])
-        continue
-    dfs(S, Times[S-1])
-    print("정답", maxArr[W])
+
+    for n in range(1,N+1):
+        if degree[n]==0:
+            deq.append(n)
+            visited[n] = timeToBuild[n-1]
+
+    while deq:
+        pNode = deq.popleft()
+        answer += timeToBuild[pNode-1]
+        for node in board[pNode]:
+            degree[node] -= 1
+            visited[node] = max(visited[node],visited[pNode]+timeToBuild[node-1]) 
+            if degree[node] == 0:
+                deq.append(node)
+
+    print(visited[W])
+
+
+    
